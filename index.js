1c2c406ac1c21dc87f68a65b673fd1e844ddca63
SakshamPhuyal <phuyalsaksham1@gmail.com>
2026-01-18 15:46:18 +0545

    authentication completed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require('express');
 const mongoose=require("mongoose");
 const path=require('path');
 const userRoute=require('./routes/user');
+const cookieParser=require('cookie-parser');
+const { checkForAuthenticationCookie } = require('./middleware/authentication');
 const app=express();
 const port=8000;
 mongoose.connect('mongodb://localhost:27017/blogify')
@@ -9,8 +11,11 @@ mongoose.connect('mongodb://localhost:27017/blogify')
 app.set('view engine','ejs');
 app.set('views',path.resolve("./views"));
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie({'token'});
 app.get('/',(req,res)=>{
     res.render("home");
+    user:req.user;
 })
 app.use('/user',userRoute);
 app.listen(port, ()=>console.log(`Server started at port:${port}`));
